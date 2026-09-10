fd02cfb8149cea2783d5713ac53ebe9482335e99
Levan <levanliparteliani@gmail.com>
2026-09-10 23:05:59 +0400

    Add Flask app factory and main blueprint

---
+++ app/main/routes.py
@@ -0,0 +1,14 @@
+from flask import Blueprint
+
+
+main_bp = Blueprint("main", __name__)
+
+
+@main_bp.route("/")
+def home():
+    return "JobBoard is running!"
+
+
+@main_bp.route("/about")
+def about():
+    return "About JobBoard"
